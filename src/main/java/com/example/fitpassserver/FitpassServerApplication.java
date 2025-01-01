83d1985129507c956f303abaa2d4e79851fe9fda
Jeongmo Seo <wjdah1402@gmail.com>
2025-01-01 20:48:57 +0900

    :rocket: chore(setting): 스프링 부트 초기 세팅

---
+++ src/main/java/com/example/fitpassserver/FitpassServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.fitpassserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitpassServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FitpassServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/fitpassserver/FitpassServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fitpassserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitpassServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
