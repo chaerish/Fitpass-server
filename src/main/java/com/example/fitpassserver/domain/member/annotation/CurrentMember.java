ad7dd7b119bba46f24d1cdb4fe6718bde38df276
leeeemj <leemj2944@gmail.com>
2025-01-12 17:12:12 +0900

    :recycle: refactor(member): @CurrentMember 등록

--- src/main/java/com/example/fitpassserver/domain/member/annotation/CurrentMember.java
+++ src/main/java/com/example/fitpassserver/domain/member/annotation/CurrentMember.java
@@ -2,10 +2,7 @@ package com.example.fitpassserver.domain.member.annotation;
 
 import io.swagger.v3.oas.annotations.Parameter;
 
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
+import java.lang.annotation.*;
 
 @Target({ElementType.PARAMETER})
 @Retention(RetentionPolicy.RUNTIME)
