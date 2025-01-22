55e9fa610b5e69b31fddf8423a4ba1f4d48b6c7b
chaerish <ccyy8432@naver.com>
2025-01-23 04:17:02 +0900

    :sparkles: feat(PayMentStatus): 결제 진행 중 Enum 추가

--- src/main/java/com/example/fitpassserver/domain/coinPaymentHistory/entity/PaymentStatus.java
+++ src/main/java/com/example/fitpassserver/domain/coinPaymentHistory/entity/PaymentStatus.java
@@ -1,6 +1,7 @@
 package com.example.fitpassserver.domain.coinPaymentHistory.entity;
 
 public enum PaymentStatus {
+    READY("결제 진행 중"),
     SUCCESS("결제 성공"),
     CANCEL("결제 취소"),
     FAIL("결제 실패");
