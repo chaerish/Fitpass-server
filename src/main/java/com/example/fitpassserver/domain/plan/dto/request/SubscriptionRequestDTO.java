657af1afd7a9ea05133c905d1a26c633a65669d9
chaerish <ccyy8432@naver.com>
2025-01-24 15:57:20 +0900

    :sparkles: bug(Plan): Plan 정기 결제 변수값 오류 수정

--- src/main/java/com/example/fitpassserver/domain/plan/dto/request/SubscriptionRequestDTO.java
+++ src/main/java/com/example/fitpassserver/domain/plan/dto/request/SubscriptionRequestDTO.java
@@ -4,7 +4,7 @@ public record SubscriptionRequestDTO(
         String cid,
         String sid,
         String partner_order_id,
-        String user_order_id,
+        String partner_user_id,
         String item_name,
         Integer quantity,
         Integer total_amount,
