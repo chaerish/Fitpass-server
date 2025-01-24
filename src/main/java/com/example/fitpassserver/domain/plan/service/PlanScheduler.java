d67e54b2c0050df7342d27400c72ff0bf9f4b582
chaerish <ccyy8432@naver.com>
2025-01-24 14:48:27 +0900

    :sparkles: feat(PlanScheduler): 정기 결제 상태를 위한 스케쥴러 추가
    테스트를 위해 fixedDelay 를 1분으로 잡아둔다. 기존은 자정 실행

---
+++ src/main/java/com/example/fitpassserver/domain/plan/service/PlanScheduler.java
@@ -0,0 +1,51 @@
+package com.example.fitpassserver.domain.plan.service;
+
+import com.example.fitpassserver.domain.coinPaymentHistory.service.CoinPaymentHistoryService;
+import com.example.fitpassserver.domain.coinPaymentHistory.service.KakaoPaymentService;
+import com.example.fitpassserver.domain.member.entity.Member;
+import com.example.fitpassserver.domain.plan.dto.response.SubscriptionResponseDTO;
+import com.example.fitpassserver.domain.plan.entity.Plan;
+import com.example.fitpassserver.domain.plan.repository.PlanRepository;
+import java.time.LocalDate;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+@Component
+@Slf4j
+@RequiredArgsConstructor
+public class PlanScheduler {
+    private final PlanRepository planRepository;
+    private final KakaoPaymentService paymentService;
+    private final CoinPaymentHistoryService coinPaymentHistoryService;
+
+    //    @Scheduled(cron = "0 0 0 * * ?")
+    @Scheduled(fixedDelay = 60000)
+    @Async
+    public void regularPay() {
+        List<Plan> planToProcess = planRepository.findAllByPlanDateBefore(LocalDate.now());
+        for (Plan plan : planToProcess) {
+            try {
+                processPay(plan);
+            } catch (Exception e) {
+                log.error("plan ID: {} 에서 정기 결제 실패: {}", plan.getId(), e.getMessage(), e);
+                throw e;
+            }
+        }
+    }
+
+    @Transactional
+    public void processPay(Plan plan) {
+        Member member = plan.getMember();
+        SubscriptionResponseDTO response = paymentService.ready(plan);
+        coinPaymentHistoryService.createNewCoinHistory(member, response.tid());
+        plan.updatePlanDate();
+        plan.addPaymentCount();
+        planRepository.save(plan);
+    }
+
+}
