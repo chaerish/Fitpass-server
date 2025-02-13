c057b87f2c78b7dc89d989b603e6b0f532d4ecae
chaerish <ccyy8432@naver.com>
2025-02-13 22:23:08 +0900

    :recycle: refactor(Fitness): 피트니스 상태 추가

--- src/main/java/com/example/fitpassserver/domain/fitness/entity/Status.java
+++ src/main/java/com/example/fitpassserver/domain/fitness/entity/Status.java
@@ -1,5 +1,15 @@
 package com.example.fitpassserver.domain.fitness.entity;
 
 public enum Status {
-    NONE, PROGRESS, DONE, REVIEWED
+    NONE("사용 안함"), PROGRESS("사용 중"), DONE("사용 불가"), REVIEWED("사용 불가(리뷰 작성 완료)");
+
+    Status(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    private String value;
 }
