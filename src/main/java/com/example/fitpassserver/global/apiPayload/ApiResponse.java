fd61b7e465e7d909ce161b2a989f26c6f3961bdf
chaerish <ccyy8432@naver.com>
2025-02-13 22:22:25 +0900

    :recycle: refactor(ApiResponse): api 응답에 빌더 추가

--- src/main/java/com/example/fitpassserver/global/apiPayload/ApiResponse.java
+++ src/main/java/com/example/fitpassserver/global/apiPayload/ApiResponse.java
@@ -7,9 +7,11 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 
 @Getter
+@Builder
 @AllArgsConstructor
 @JsonPropertyOrder({"isSuccess", "code", "message", "result"})
 public class ApiResponse<T> {
@@ -22,15 +24,24 @@ public class ApiResponse<T> {
     private T result;
 
 
-    public static <T> ApiResponse<T> onSuccess(T result){
+    public static <T> ApiResponse<T> onSuccess(T result) {
         ReasonDTO dto = SuccessCode._OK.getReasonHttpStatus();
         return new ApiResponse<>(true, dto.getCode(), dto.getMessage(), result);
     }
-    public static <T> ApiResponse<T> of(BaseCode code, T result){
-            return new ApiResponse<>(true, code.getReasonHttpStatus().getCode() , code.getReasonHttpStatus().getMessage(), result);
+
+    public static <T> ApiResponse<T> of(BaseCode code, T result) {
+        return new ApiResponse<>(true, code.getReasonHttpStatus().getCode(), code.getReasonHttpStatus().getMessage(),
+                result);
     }
 
-    public static <T> ApiResponse<T> onFailure(String code, String message, T data){
+    public static <T> ApiResponse<T> onFailure(String code, String message, T data) {
         return new ApiResponse<>(false, code, message, data);
+    }
+
+    public static <T> ApiResponse<T> onFailure(String code, String message) {
+        return ApiResponse.<T>builder()
+                .code(code)
+                .message(message)
+                .build();
     }
 }
