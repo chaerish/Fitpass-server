38e371748546cbff6e64bca1d82de06b926bddd1
leeeemj <leemj2944@gmail.com>
2025-02-14 13:07:27 +0900

    :bug: bug: 충돌 해결

--- src/main/java/com/example/fitpassserver/domain/coinPaymentHistory/service/CoinPaymentHistoryService.java
+++ src/main/java/com/example/fitpassserver/domain/coinPaymentHistory/service/CoinPaymentHistoryService.java
@@ -56,6 +56,25 @@ public class CoinPaymentHistoryService {
                 .build());
     }
 
+    public CoinPaymentHistory createNewCoinPaymentByScheduler(Member member, SubscriptionResponseDTO dto) {
+        PlanType type = PlanType.getPlanType(dto.item_name());
+        if (type == null) {
+            throw new PlanException(PlanErrorCode.PLAN_NOT_FOUND);
+        }
+        PlanTypeEntity planType = planTypeRepository.findByPlanType(type)
+                .orElseThrow(() -> new PlanException(PlanErrorCode.PLAN_NOT_FOUND));
+        
+        return coinPaymentRepository.save(CoinPaymentHistory.builder()
+                .paymentMethod("카카오페이 정기 결제") //todo: 수정
+                .isAgree(true)
+                .tid(dto.tid())
+                .coinCount((long) planType.getCoinQuantity())
+                .paymentStatus(PaymentStatus.READY)
+                .member(member)
+                .paymentPrice(dto.amount().total())
+                .build());
+    }
+
     public void createNewCoinPaymentByPlan(Member member, String tid, PlanSubScriptionRequestDTO dto) {
         PlanType type = PlanType.getPlanType(dto.itemName());
         if (type == null) {
@@ -72,24 +91,6 @@ public class CoinPaymentHistoryService {
                 .paymentStatus(PaymentStatus.READY)
                 .member(member)
                 .paymentPrice(dto.totalAmount())
-                .build());
-    }
-
-    public CoinPaymentHistory createNewCoinPaymentByScheduler(Member member, SubscriptionResponseDTO dto) {
-        PlanType type = PlanType.getPlanType(dto.item_name());
-        if (type == null) {
-            throw new PlanException(PlanErrorCode.PLAN_NOT_FOUND);
-        }
-        PlanTypeEntity planType = planTypeRepository.findByPlanType(type)
-                .orElseThrow(() -> new PlanException(PlanErrorCode.PLAN_NOT_FOUND));
-        return coinPaymentRepository.save(CoinPaymentHistory.builder()
-                .paymentMethod("카카오페이 정기 결제") //todo: 수정
-                .isAgree(true)
-                .tid(dto.tid())
-                .coinCount((long) planType.getCoinQuantity())
-                .paymentStatus(PaymentStatus.READY)
-                .member(member)
-                .paymentPrice(dto.amount().total())
                 .build());
     }
 
