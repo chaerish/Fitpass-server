0b6d03dbd52da13e193df2363fac4e5427c399ef
김채연 <100480440+chaerish@users.noreply.github.com>
2025-02-18 01:04:12 +0900

    Update SecurityConfig.java

--- src/main/java/com/example/fitpassserver/global/config/SecurityConfig.java
+++ src/main/java/com/example/fitpassserver/global/config/SecurityConfig.java
@@ -102,7 +102,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:5173", "https://localhost:5173", "https://api.fitpass.co.kr", "https://www.fitpass.co.kr", "https://fitpass.co.kr", "https://fitpass-web.vercel.app"));
+        configuration.setAllowedOrigins(List.of("http://localhost:5173", "https://localhost:5173", "https://api.fitpass.co.kr", "https://fitpass.co.kr", "https://fitpass-web.vercel.app"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setExposedHeaders(List.of("*"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
