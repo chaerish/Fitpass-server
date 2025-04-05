9b1c58aafab43f59d1a6dab6703a646413497a89
JuHyuk Lim <nyclo0705@gmail.com>
2025-04-05 23:34:44 +0900

    :sparkles: feat(dashboardOwner): 이번 달 정산 금액 조회 기능 추가

--- src/main/java/com/example/fitpassserver/owner/dashboard/controller/DashboardOwnerController.java
+++ src/main/java/com/example/fitpassserver/owner/dashboard/controller/DashboardOwnerController.java
@@ -4,11 +4,13 @@ import com.example.fitpassserver.domain.member.annotation.CurrentMember;
 import com.example.fitpassserver.domain.member.entity.Member;
 import com.example.fitpassserver.global.apiPayload.ApiResponse;
 import com.example.fitpassserver.owner.dashboard.service.DashboardOwnerService;
+import com.example.fitpassserver.owner.fitnessHistory.dto.RevenueHistoryResponseDTO;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,9 +23,19 @@ public class DashboardOwnerController {
     private final DashboardOwnerService dashboardOwnerService;
 
     @Operation(summary = "공지사항 3개 조회 API", description = "공지사항 3개 조회합니다.")
-    @GetMapping
+    @GetMapping("/notices")
     public ApiResponse<?> getNotices(@CurrentMember Member member) {
         Map<String, Object> notices = dashboardOwnerService.getNotices();
         return ApiResponse.onSuccess(notices);
+    }
+
+    @Operation(summary = "이번 달 정산금액 조회 API", description = "이번 달 정산 금액을 조회합니다.")
+    @GetMapping("/settlements/month/{fitnessId}")
+    public ApiResponse<?> getMonthSettlement(
+            @CurrentMember Member member,
+            @PathVariable Long fitnessId
+    ) {
+        RevenueHistoryResponseDTO res = dashboardOwnerService.getFitnessMonthRevenue(member, fitnessId);
+        return ApiResponse.onSuccess(res);
     }
 }
--- src/main/java/com/example/fitpassserver/owner/dashboard/service/DashboardOwnerService.java
+++ src/main/java/com/example/fitpassserver/owner/dashboard/service/DashboardOwnerService.java
@@ -1,7 +1,11 @@
 package com.example.fitpassserver.owner.dashboard.service;
 
+import com.example.fitpassserver.domain.member.entity.Member;
+import com.example.fitpassserver.owner.fitnessHistory.dto.RevenueHistoryResponseDTO;
 import java.util.Map;
 
 public interface DashboardOwnerService {
     Map<String, Object> getNotices();
+
+    RevenueHistoryResponseDTO getFitnessMonthRevenue(Member member, Long fitnessId);
 }
