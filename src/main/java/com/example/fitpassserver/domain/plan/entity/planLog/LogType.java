03064dac17ee05975e1771b9bdd2461a229c6cb5
chaerish <ccyy8432@naver.com>
2025-04-20 17:59:14 +0900

    :sparkles: feat(NotificationLog): 결제 실패, 취소 시 알림톡 발생 로그 구현

---
+++ src/main/java/com/example/fitpassserver/domain/plan/entity/planLog/LogType.java
@@ -0,0 +1,5 @@
+package com.example.fitpassserver.domain.plan.entity.planLog;
+
+public enum LogType {
+    FAIL, CANCEL
+}
---
+++ src/main/java/com/example/fitpassserver/domain/plan/service/planLog/NotificationLogService.java
@@ -0,0 +1,37 @@
+package com.example.fitpassserver.domain.plan.service.planLog;
+
+import com.example.fitpassserver.domain.plan.entity.planLog.LogType;
+import com.example.fitpassserver.domain.plan.entity.planLog.NotificationLog;
+import com.example.fitpassserver.domain.plan.repository.NotificationLogRepository;
+import java.time.LocalDateTime;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class NotificationLogService {
+    private final NotificationLogRepository notificationLogRepository;
+
+    // 알림톡 동시에 객체 생성
+    public NotificationLog createNewNotification(LogType logType) {
+        NotificationLog notificationLog = NotificationLog.builder()
+                .sentAt(LocalDateTime.now())
+                .logType(logType)
+                .isSent(true)
+                .build();
+        notificationLogRepository.save(notificationLog);
+        return notificationLog;
+    }
+
+    // 실패 알림톡 동시에  생성
+    public NotificationLog createFailNotification(LogType logType) {
+        NotificationLog notificationLog = NotificationLog.builder()
+                .sentAt(LocalDateTime.now())
+                .logType(logType)
+                .isSent(false)
+                .build();
+        notificationLogRepository.save(notificationLog);
+        return notificationLog;
+    }
+
+}
